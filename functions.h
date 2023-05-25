e09efab23c15951775cba50065c19112debfe5e6
KristekC <134609829+KristekC@users.noreply.github.com>
2023-05-25 17:27:07 +0200

    functions.h

---
+++ functions.h
@@ -0,0 +1,15 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+
+//10
+
+int	 menu(const char* const);
+void kreiranjeDatoteke(const char* const);
+void dodavanjeAlata(const char* const);
+void* ucitajAlate(const char* const);
+void ispisiSve(const ALAT*);
+void ispisiSortirano(const ALAT*);
+void brisanjeAlata(ALAT* const, const char* const);
+
+#endif

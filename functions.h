feea5d0dbc1fb0f07557e620a5ecac8022ff7da1
KristekC <134609829+KristekC@users.noreply.github.com>
2023-05-29 14:06:45 +0200

    Update functions.h

--- functions.h
+++ functions.h
@@ -10,6 +10,8 @@ void dodavanjeAlata(const char* const);
 void* ucitajAlate(const char* const);
 void ispisiSve(const ALAT*);
 void ispisiSortirano(const ALAT*);
+void uredivanjeAlata(ALAT* const, const char* const);
 void brisanjeAlata(ALAT* const, const char* const);
+void* pretraziAlate(ALAT* const);
 
 #endif

bc4096a060d9cf7fcdc0a18a2d9d685ba4238c65
KristekC <134609829+KristekC@users.noreply.github.com>
2023-06-01 16:54:25 +0200

    Update izbornik.c

--- izbornik.c
+++ izbornik.c
@@ -6,105 +6,90 @@
 #include "functions.h"
 #include "dataType.h"
 
-//6 (više datoteka)
-//8
-//14
-int main() {
 
+//8
+int main() {
 	int odabir = 0;
+	char ime[100];
 
 	printf("__________________________________________________________________\n");
 	printf("Izaberite sto zelite napraviti unosom broja opcije:\n\n");
-
-	printf("1	Prikazi sve alate\n");
-	printf("2	Prikazi cijenu zeljenog alata\n");
-	printf("3	Prikazi alate sortirane po cijeni od najnize do najvise\n");
-	printf("4	Promjeni kolicinu nekog alata u skladistu\n");
-	printf("5	Brisanje nekog alata iz evidencije\n");
-	printf("6	Dodavanje nekog alata u evidenciju\n");
-
+	printf("1   Prikazi sve alate\n");
+	printf("2   Prikazi cijenu zeljenog alata\n");
+	printf("3   Prikazi alate sortirane po cijeni od najnize do najvise\n");
+	printf("4   Promjeni kolicinu nekog alata u skladistu\n");
+	printf("5   Brisanje nekog alata iz evidencije\n");
+	printf("6   Dodavanje nekog alata u evidenciju\n");
 	printf("__________________________________________________________________\n");
-
+	 //4
 	scanf("%d", &odabir);
 
-	static ALAT* poljeAlata = NULL;
+	ALAT* poljeAlata = NULL;
 
-	switch (odabir)
-	{
+	switch (odabir) {
 	case 1:
-		if (poljeAlata != NULL)
-		{
+		if (poljeAlata != NULL) {
 			free(poljeAlata);
 			poljeAlata = NULL;
 		}
 
-		poljeAlata = (ALAT*)ucitajAlat(ime);
+		poljeAlata = ucitajAlat(ime);
 
 		ispisiSve(poljeAlata);
 		break;
-	}
 
 	case 2:
-		if (poljeAlata != NULL)
-		{
+		if (poljeAlata != NULL) {
 			free(poljeAlata);
 			poljeAlata = NULL;
 		}
 
-		poljeAlata = (ALAT*)ucitajAlate(ime);
+		poljeAlata = ucitajAlate(ime);
 
-		pronadenAlat = (ALAT*)pretraziAlate(poljeAlata);
+		ALAT* pronadenAlat = pretraziAlate(poljeAlata);
 
-		if (pronadenAlat != NULL)
-		{
+		if (pronadenAlat != NULL) {
 			printf("\n");
 			printf("ID: %d\n", pronadenAlat->id);
 			printf("Naziv: %s\n", pronadenAlat->naziv);
 			printf("Kolicina: %d\n", pronadenAlat->kolicina);
 			printf("Cijena: %d kn\n", pronadenAlat->cijena);
 		}
-		else
-		{
+		else {
 			printf("Ne postoji alat s tim ID-em.\n");
-
 		}
 
 		break;
 
 	case 3:
-		if (poljeAlata != NULL)
-		{
+		if (poljeAlata != NULL) {
 			free(poljeAlata);
 			poljeAlata = NULL;
 		}
 
-		poljeAlata = (ALAT*)ucitajAlate(ime);
+		poljeAlata = ucitajAlate(ime);
 
 		ispisiSortirano(poljeAlata, 2);
 		break;
 
-}
-
 	case 5:
-		if (poljeAlata != NULL)
-		{
+		if (poljeAlata != NULL) {
 			free(poljeAlata);
 			poljeAlata = NULL;
 		}
 
-		poljeAlata = (ALAT*)ucitajAlat(ime);
+		poljeAlata = ucitajAlat(ime);
 
 		dodavanjeAlata(poljeAlata, ime);
 		break;
 
 	case 6:
-		if (poljeAlata != NULL)
-		{
+		if (poljeAlata != NULL) {
 			free(poljeAlata);
 			poljeAlata = NULL;
 		}
 
-		poljeAlata = (ALAT*)ucitajAlate(ime);
+		poljeAlata = ucitajAlate(ime);
 
 		brisanjeAlata(poljeAlata, ime);
 		break;
